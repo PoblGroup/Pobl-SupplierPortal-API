362102e489f6c886c8c486c26d8efd1dba767ffc
Rich Griffiths <rich.griffiths89@gmail.com>
2022-01-25 15:38:06 +0000

    new variation object

--- controllers/variationController.js
+++ controllers/variationController.js
@@ -1,29 +1,34 @@
 const variationData = require('../data/variations')
 
 const createVariation = async (req, res) => {
-    res.json({message: "Test"})
-
     try {
-        const { MaintenanceJobVariation } = req.body
+        const newVariation = { 
+            JobReference,
+            DateTime,
+            VariationType,
+            SequenceNumber,
+            Product,
+            WriteInProduct,
+            Unit,
+            PricePerUnit,
+            Quantity,
+            TotalAmount,
+            SubLocation,
+            Note,
+            Direction
+        } = req.body
 
-        const newVariation = {
-            JobReference: MaintenanceJobVariation.JobReference,
-            Date: MaintenanceJobVariation.DateTime,
-            SequenceNo: MaintenanceJobVariation.SequenceNo,
-            Action: MaintenanceJobVariation.Action,
-            Product: MaintenanceJobVariation.Product,
-            SubLocation: MaintenanceJobVariation.SubLocation,
-            Quantity: MaintenanceJobVariation.Quantity,
-            JobTotalValue: MaintenanceJobVariation.JobTotalValue,
-            Direction: MaintenanceJobVariation.Direction.toUpperCase(),
-            CreatedOn: new Date().toISOString().slice(0, 19).replace('T', ' '),
-            ModifiedOn: new Date().toISOString().slice(0, 19).replace('T', ' ')
-        } 
+        newVariation.SubLocation = (SubLocation == null) ? null : SubLocation.toUpperCase(),
+        newVariation.Direction = (Direction == null) ? "I" : Direction.toUpperCase(),
+        newVariation.CreatedOn = new Date().toISOString().slice(0, 19).replace('T', ' '),
+        newVariation.ModifiedOn = new Date().toISOString().slice(0, 19).replace('T', ' ')
+        
+        console.log(newVariation)
         
         const created = await variationData.createVariation(newVariation)
-        // res.send(created)
-        console.log(newVariation)
-        console.log(created)
+        res.send(created)
+
+
     } catch (error) {
         res.status(400).send(error.message)
     }
--- data/variations/index.js
+++ data/variations/index.js
@@ -15,18 +15,23 @@ const sql = require('mssql')
 // }
 
 const createVariation = async (variation) => {
+
     try {
         let pool = await sql.connect(config.sql)
         const sqlQueries = await utils.loadSqlQueries('variations')
         const insertVariation = await pool.request()
             .input ('JobReference', sql.NVarChar(50), variation.JobReference)
-            .input ('DateTime', sql.DateTime, variation.Date)
-            .input ('SequenceNo', sql.Int, variation.SequenceNo) 
-            .input ('Action', sql.NVarChar(50), variation.Action)
+            .input ('DateTime', sql.DateTime, variation.DateTime)
+            .input ('VariationType', sql.NVarChar(50), variation.VariationType)
+            .input ('SequenceNumber', sql.Int, variation.SequenceNumber) 
             .input ('Product', sql.NVarChar(250), variation.Product)
+            .input ('WriteInProduct', sql.NVarChar(500), variation.WriteInProduct)
+            .input ('Unit', sql.NVarChar(250), variation.Unit)
+            .input ('PricePerUnit', sql.Decimal, variation.PricePerUnit)
+            .input ('Quantity', sql.Decimal, variation.Quantity)
+            .input ('TotalAmount', sql.Decimal, variation.TotalAmount)
             .input ('SubLocation', sql.NVarChar(250), variation.SubLocation)
-            .input ('Quantity', sql.Int, variation.Quantity)
-            .input ('JobTotalValue', sql.Decimal, variation.JobTotalValue)
+            .input ('Note', sql.NVarChar(250), variation.Note)
             .input ('Direction', sql.NChar(1), variation.Direction)
             .input ('CreatedOn', sql.DateTime, new Date(variation.CreatedOn))
             .input ('ModifiedOn', sql.DateTime, new Date(variation.ModifiedOn))
